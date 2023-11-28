7a46f375c8c34c6c070ee83d20edcbe4bb42e5df
BrennanTanner <brenntanner@gmail.com>
2023-11-28 00:02:50 -0700

    removed redundant code

--- src/app/api/notify-beacon/route.ts
+++ src/app/api/notify-beacon/route.ts
@@ -4,12 +4,10 @@ import webpush from 'web-push';
 
 export async function POST(request: NextRequest) {
    const data = await request.json();
-   const _PrivateKey_ = process.env.NEXT_PUBLIC_VAPID_PRIVATE_KEY;
-   const _PublicKey_ = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY;
 
    //needs to just take array of endpoints and distribute notifications to each
 
-   webpush.setVapidDetails('mailto:test@test.test', _PublicKey_, _PrivateKey_);
+   webpush.setVapidDetails('mailto:test@test.test', process.env.NEXT_PUBLIC_VAPID_PRIVATE_KEY, process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY);
 
    const notification = JSON.stringify({
       title: 'Hello, Notifications!',
@@ -17,17 +15,6 @@ export async function POST(request: NextRequest) {
          body: `ID: 100`,
       },
    });
-
-   const subscription = {
-      endpoint:
-         'https://fcm.googleapis.com/fcm/send/cupot3r9RKc:APA91bGV7x0OGUjO7XkLz8NTx7GvxySBi6KvcohfzTgPoCIe7hceYewqFTImkOAT-kS4Q5R0K8jU-fnukpKD-oSNPLQ_QQ83wM_dXfDMtBApwE1CsuDH_CtZR27rZjVwgOUorWA7Vhhm',
-      keys: {
-         auth: 'QYi3yTv-QtVkQ_LtKzVLrw',
-         p256dh:
-            'BJHI-gjWVnqsXaxpJmPoLNbNK950gVjj9eIt7ntZOfVn-yWDik6ToVCuVNRiIoo4vNa55e3WNwWSqVOXtIHZFq4',
-      },
-      expiration_time: null,
-   };
 
    const results = data.subscriptions.forEach(async (subscription) => {
       const endpoint = subscription.endpoint;
