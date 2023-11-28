dfdfe96837a008cbfe53c05a5432ef7778144b85
BrennanTanner <brenntanner@gmail.com>
2023-11-28 10:20:46 -0700

    hmmm

--- src/app/api/notify-beacon/route.ts
+++ src/app/api/notify-beacon/route.ts
@@ -7,7 +7,11 @@ export async function POST(request: NextRequest) {
 
    //needs to just take array of endpoints and distribute notifications to each
 
-   webpush.setVapidDetails('mailto:test@test.test', process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY, process.env.NEXT_PUBLIC_VAPID_PRIVATE_KEY);
+   webpush.setVapidDetails(
+      'mailto:test@test.test',
+      process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
+      process.env.NEXT_PUBLIC_VAPID_PRIVATE_KEY
+   );
 
    const notification = JSON.stringify({
       title: 'Hello, Notifications!',
@@ -16,25 +20,33 @@ export async function POST(request: NextRequest) {
       },
    });
 
-   console.log(data.subscriptions);
-   const results = data.subscriptions.forEach(async (subscription) => {
-      const endpoint = subscription.endpoint;
-      const id = endpoint.substr(endpoint.length - 8, endpoint.length);
-      const results = { success: [], fail: [] };
+   //console.log(data.subscriptions);
+   //const results = { success: [], fail: [] }
+   const results = data.subscriptions.map((subscription) => {
+      if (subscription && subscription != 'null') {
+         const endpoint = subscription.endpoint;
+         const id = endpoint.substr(endpoint.length - 8, endpoint.length);
+         webpush
+            .sendNotification(subscription, notification)
+            .then(async (result) => {
 
-      await webpush
-         .sendNotification(subscription, notification)
-         .then((result) => {
-            results.success.push({ id: id, result: result });
-         })
-         .catch((error) => {
-            return results.fail.push({ id: id, result: error });
-         });
+               
+               //results.success.push({ id: id, result: result });
+               return result.status;
+            })
+            .catch((error) => {
+               //results.fail.push({ id: id, result: error });
+               return error;
+            });
+            return '201';
+      } else {
+         return '404';
+      }
    });
 
    return NextResponse.json(
       {
-         results: results,
+         results: await results,
       },
       {
          status: 201,
