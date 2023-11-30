ec831f4c68367c1badd6803df98b248f2960b1ca
BrennanTanner <brenntanner@gmail.com>
2023-11-30 12:30:35 -0700

    fixed manifest

--- src/app/seo.jsx
+++ src/app/seo.jsx
@@ -4,7 +4,6 @@ const SEO = () => (
    <Head>
       {/* <title>{pageTitle}</title>
       <meta name='description' content={pageDescription} /> */}
-      <link rel='manifest' href='/site.webmanifest' />
       <link
          rel='apple-touch-icon'
          sizes='180x180'
@@ -22,7 +21,7 @@ const SEO = () => (
          sizes='16x16'
          href='/favicon-16x16.png'
       />
-      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='manifest' href='/manifest.webmanifest' />
       <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
       <meta name='msapplication-TileColor' content='#da532c' />
       <meta name='theme-color' content='#ffffff' />
