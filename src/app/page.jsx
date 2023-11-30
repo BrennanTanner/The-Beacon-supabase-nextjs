a5d86be2fdef5a6504319b293635e3b96718021b
BrennanTanner <brenntanner@gmail.com>
2023-11-30 12:04:20 -0700

    added manifest file and head tag

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import Navbar from './components/Navbar';
 import BeaconCarousel from './components/Beacon/beaconCarousel';
+import SEO from './seo';
 
 export default async function Home() {
    const cookieStore = cookies();
@@ -13,10 +14,13 @@ export default async function Home() {
 
    if (session) {
       return (
+         <div>
+         <SEO pageTitle="The Beacon" pageDescription="Notify your friends with the Beacon!"></SEO>
          <main style={{ backgroundImage: 'url("/space-w-alpha.png")' }}>
             <Navbar session={session} />
             <BeaconCarousel session={session} />
          </main>
+         </div>
       );
    } else {
       return <AuthForm />;
